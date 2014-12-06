4840198d62e7fbca7432ff9e84186d6ff8d0a2f3
chinarosten <luhangyu2000@163.com>
2014-12-06 16:00:10 +0800

    初步调整

--- grails-app/domain/com/rosten/app/bargain/UndertakeBargain.groovy
+++ grails-app/domain/com/rosten/app/bargain/UndertakeBargain.groovy
@@ -1,8 +1,10 @@
 package com.rosten.app.bargain
+
 import java.util.Date;
 import java.text.SimpleDateFormat
 import com.rosten.app.annotation.GridColumn
 import com.rosten.app.system.Company
+import com.rosten.app.base.ContactCorp
 
 
 class UndertakeBargain {
@@ -24,41 +26,22 @@ class UndertakeBargain {
 	@GridColumn(name="合同号",colIdx=3,width="60px")
 	String undertakeBargainNo
 	
-	
 	//合同金额
 	@GridColumn(name="合同金额",colIdx=6)
 	Long undertakeBargainMoney
-	//合同甲方
-	@GridColumn(name="合同甲方",colIdx=4,width="220px")
-	String undertakeBargainVendor
-	//合同甲方单位名称
-	String undertakeBargainVendorCropName
-	//合同甲方单位地址
-	String undertakeBargainVendorAddress
-	//合同甲方邮编
-	String undertakeBargainVendorPost
-	//合同甲方单位电话
-	String undertakeBargainVendorPhone
-	//合同甲方单位法人
-	String undertakeBargainVendorBoss
-	//合同甲方单位法人职务
-	String undertakeBargainVendorDuty
 	
+	//合同甲方
+	ContactCorp undertakeBargainVendor
+	@GridColumn(name="合同甲方",colIdx=4,width="220px")
+	def getContactCorpName(){
+		return undertakeBargainVendor?.contactCorpName
+	}
+	
+		
 	//合同乙方
 	@GridColumn(name="合同乙方",colIdx=5,width="220px")
-	String undertakeBargainPurchaser
-	//合同乙方单位名称
-	String undertakeBargainPurchaserCropName
-	//合同乙方单位地址
-	String undertakeBargainPurchaserAddress
-	//合同乙方邮编
-	String undertakeBargainPurchaserPost
-	//合同乙方单位电话
-	String undertakeBargainPurchaserPhone
-	//合同乙方单位法人
-	String undertakeBargainPurchaserBoss
-	//合同乙方单位法人职务
-	String undertakeBargainPurchaserDuty
+	ContactCorp undertakeBargainPurchaser
+	
 	
 	//制表人
 	String undertakeBargainMaker
