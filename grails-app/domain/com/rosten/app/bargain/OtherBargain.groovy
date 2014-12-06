341ab5c7d29e473a56d213e4129270c8089e8ba2
tiagoxu <xkf1634925447>
2014-12-06 19:58:15 +0800

    xkfpchome-删除合同多余class

--- grails-app/domain/com/rosten/app/bargain/OtherBargain.groovy
+++
@@ -1,9 +0,0 @@
-package com.rosten.app.bargain
-import java.util.Date;
-import com.rosten.app.annotation.GridColumn
-import com.rosten.app.system.Company
-class OtherBargain {
-
-    static constraints = {
-    }
-}
